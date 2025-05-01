a02d1f28bc3809cf61a0d874fa6904c1a8842537
ValAnn <valovaanna04@mail.ru>
2025-05-01 23:04:23 +0400

    init

---
+++ src/test/java/com/example/house_services/HouseServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.house_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
